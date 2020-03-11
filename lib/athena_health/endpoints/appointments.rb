0720fe3a0c8c4154a36112a9aae4abd11cc2fdd8
Noskov Gleb <g.noskov@sloboda-studio.com>
2020-03-11 13:29:35 +0200

    add additional parameter for creating appointment note

--- lib/athena_health/endpoints/appointments.rb
+++ lib/athena_health/endpoints/appointments.rb
@@ -112,11 +112,14 @@ module AthenaHealth
         NoteCollection.new(response)
       end
 
-      def create_appointment_note(practice_id:, appointment_id:, note_text:)
+      def create_appointment_note(practice_id:, appointment_id:, note_text:, displayonschedule: false)
         @api.call(
           endpoint: "#{practice_id}/appointments/#{appointment_id}/notes",
           method: :post,
-          body: { notetext: note_text }
+          body: {
+            notetext: note_text,
+            displayonschedule: displayonschedule
+          }
         )
       end
 
